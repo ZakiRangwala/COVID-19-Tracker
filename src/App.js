cf35df92afcf8201c02e3c5835c8f424cb23789a
Zaki Rangwala <zakirangwala@gmail.com>
2020-11-02 07:05:43 -0500

    fetch country data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
-
+import "./App.css";
 function App() {
-  const [countries, setCountries] = useState(["USA", "UK", "INDIA"]); //variable and modifiers initialized into an array
+  const [countries, setCountries] = useState([]); //variable and modifiers initialized into an array
+  // STATE = How to write a variable in REACT <<<<<
+  // useeffect = runs piece of code based on a condition (if statement)
 
-  //STATE = How to write a variable in REACT <<<<<
+  useEffect(() => {
+    //Code in here will run once when component loads, not again
+    //Will run each time param variable changes
+    //async -> send a request, wait for it, do something with it
+    const getCountriesData = async () => {
+      //make a promise to recieve request then do something
+      await fetch("https://disease.sh/v3/covid-19/countries") //await means to wait for Data
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            //map loops trhrough array and is mutable
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
       <div className="app__header">
@@ -13,19 +32,12 @@ function App() {
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
             {/* Loop through all the countries and find stats based on them */}
-
-            {countries.map((country) => {
-              <MenuItem value={country}>{country}</MenuItem>;
-            })}
-            
-            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">India</MenuItem>
-            <MenuItem value="worldwide">Canada</MenuItem>
-            <MenuItem value="worldwide">U.S.A</MenuItem> */}
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
-
       {/* Header */}
       {/* Title and Dropdown */}
 
